7a524bf3d075862624fcde7865a4f0fb9564b89c
tharunknkt <tharun.vtalent@gmail.com>
2018-12-22 09:44:41 +0530

    hello

--- CoreJava_Dec18/src/com/vtalent/tharun/Hello.java
+++ CoreJava_Dec18/src/com/vtalent/tharun/Hello.java
@@ -1,12 +1,11 @@
 package com.vtalent.tharun;
 
-public class Welcome {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome");
-for(int m=0;m<=2;m++){
-System.out.println(m);
+		System.out.println("hello");
+
 	}
 
-	}}
+}
