a5e3e52b8bc8ed477532d2dfaab35937d2c2ede0
Hanu07 <hanuman.vtalent@gmail.com>
2018-12-26 19:42:20 +0530

    Hello....

---
+++ CoreJava_Dec18/src/com/vtalent/hanu/Sample1.java
@@ -0,0 +1,11 @@
+package com.vtalent.hanu;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to GiT");
+
+	}
+
+}
