f6615cc4979c1b4ebdc5a6e2a950b931d933ba8c
Hanu07 <hanuman.vtalent@gmail.com>
2018-12-26 20:14:00 +0530

    Commit

--- CoreJava_Dec18/src/com/vtalent/hanu/Sample1.java
+++ CoreJava_Dec18/src/com/vtalent/hanu/Sample1.java
@@ -5,7 +5,6 @@ public class Sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to GiT");
-		System.out.println("------");
 		System.out.println();
 
 	}
