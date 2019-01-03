404a641591b5625afd721d8cf7fdb0047459451f
Chamundeswari K N Rao <chamundeswari6.vtalent@gmail.com>
2019-01-03 12:14:54 +0530

    Arrays

---
+++ CoreJava_Dec18/src/com/vtalent/chamu/Employee.java
@@ -0,0 +1,27 @@
+package com.vtalent.chamu;
+
+public class Employee 
+{
+	private int id;
+	private long mblno;
+	private double salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public long getMblno() {
+		return mblno;
+	}
+	public void setMblno(long mblno) {
+		this.mblno = mblno;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
