14691fcb3bb943a74fd31e9986d2f1081dc9aa07
santhoshkumar1122 <santhosh22.vtalent@gmail.com>
2019-01-04 09:25:08 +0530

    hi

---
+++ CoreJava_Dec18/src/com/vtalennt/Interfces/Telephone.java
@@ -0,0 +1,11 @@
+package com.vtalennt.Interfces;
+
+public interface Telephone {
+	public void powerOn();
+	public void dial(int phonenumber);
+	public void answer();
+	public boolean callPhone(int phonenumber);
+	public boolean isRinging();
+	
+
+}
