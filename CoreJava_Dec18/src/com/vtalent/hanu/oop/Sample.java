cf64cbc58c6d0316a8475cb990625b809d60212b
sujithn99 <sujithkumar.vtalent@gmail.com>
2019-01-08 20:19:06 +0530

    commit

--- CoreJava_Dec18/src/com/vtalent/hanu/oop/Sample.java
+++ CoreJava_Dec18/src/com/vtalent/hanu/oop/Sample.java
@@ -3,13 +3,14 @@ package com.vtalent.hanu.oop;
 public class Sample {
 	
 	
-	float f1,f2;
+	public float f1;
+	public float f2;
 	
-	void function(float f1,float f2){
+	public void function(float f1,float f2){
 		this.f1=f1+f2;
 		this.f2=f2+this.f2;
 	}
-	void function(long l){
+	public void function(long l){
 		this.f1=this.f1+(float)l;
 	}
 
--- CoreJava_Dec18/src/com/vtalent/hanu/oop/SampleOne.java
+++ CoreJava_Dec18/src/com/vtalent/hanu/oop/SampleOne.java
@@ -1,8 +1,8 @@
 package com.vtalent.hanu.oop;
 
 public class SampleOne extends Sample{
-	float f1;
-	long l2;
+	public float f1;
+	public long l2;
 	public void function(long l2) {
 		this.l2=this.l2+l2;
 	}
