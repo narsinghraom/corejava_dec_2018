42f6f5670d2ede5d01430effd0418e278f47afc0
Hanu07 <hanuman.vtalent@gmail.com>
2019-01-10 19:10:49 +0530

    cricket

--- CoreJava_Dec18/src/com/vtalent/hanu/classtask/Conditions.java
+++ CoreJava_Dec18/src/com/vtalent/hanu/classtask/Conditions.java
@@ -4,21 +4,34 @@ import java.util.Scanner;
 
 public class Conditions {
 	
+	static double d;
+	
 	public static double checkRead() {
 		Scanner s= new Scanner(System.in);
 		System.out.println("Enter Overs");
-		double d=s.nextDouble();
+		d=s.nextDouble();
 		if (d>50) {
 			//if (d>=49.5) {
 			System.out.println("Overs must below 50");
 			System.out.println("below 49.5");
+			
 			checkRead();
 		//}
+		}
+		if (d==0) {
+			System.out.println("Try Again..");
+			checkRead();
 		}
 		if (d>=49.6 && d<=49.9) {
 			System.out.println(d+" is invalid overs");
 			System.out.println("Overs must below 50 and below 49.5");
 			System.out.println("Enter Again....");
+			checkRead();
+		}
+		String ovr=String.valueOf(d);
+		if (ovr.charAt(ovr.length()-1)>='6' && ovr.charAt(ovr.length()-1)<='9') {
+			System.out.println(d+" is invalid overs in Cricket");
+			System.out.println("Enter Valid Overs");
 			checkRead();
 		}
 		return d;
--- CoreJava_Dec18/src/com/vtalent/hanu/classtask/Team.java
+++ CoreJava_Dec18/src/com/vtalent/hanu/classtask/Team.java
@@ -44,7 +44,4 @@ public class Team {
 	public void setRunrate(double runrate) {
 		this.runrate = runrate;
 	}
-	
-	
-
 }
