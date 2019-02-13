fcd2e62d25e7451dfbf608cdbddeb7f3d92c0cf7
jadhavswetha <swethajadhav.vtalent@gmail.com>
2019-02-13 13:24:28 +0530

    here is page to simple

--- CoreJava_Dec18/src/coreconcepts/sample.java
+++ CoreJava_Dec18/src/coreconcepts/sample.java
@@ -2,4 +2,22 @@ package coreconcepts;
 
 public class sample {
 
+	static int a,b;
+	long l1;
+	public long function (int a, int b){
+		sample . a = a + b;
+		sample . b = a * b;
+		return a + b;
+	}
+	public static sample function(){
+		sample sample = new sample();
+		sample.l1 =sample.function(2, 5);
+		return sample;
+	}
+	public static void main(String[] args){
+		sample s1 = function();
+		System.out.println(a + b);
+		System.out.println(s1.l1);
+		System.out.println(s1);
+	}
 }
