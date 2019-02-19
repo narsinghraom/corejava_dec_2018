df6dbdeb985eec8deba6125a4fd0a204334da925
saranyaN15 <saranya.vtalent@gmail.com>
2019-02-19 11:49:16 +0530

    modified

--- CoreJava_Dec18/src/com/vtalent/saranya/ConstructorEx.java
+++ CoreJava_Dec18/src/com/vtalent/saranya/ConstructorEx.java
@@ -6,11 +6,13 @@ public class ConstructorEx {
 		y=7;
 	}
 	ConstructorEx(int x){
+		this(x,x+1);
 		this.x=this.x+x;
 		y=y+x;
 		
 	}
 	ConstructorEx(int x,int y){
+		this();
 		this.x=this.x+x;
 		this.y=this.y+y;
 		
@@ -19,12 +21,12 @@ public class ConstructorEx {
 		System.out.println(x+y);
 	}
 	public static void main(String[] args){
-		ConstructorEx c1 =new ConstructorEx();
+		//*ConstructorEx c1 =new ConstructorEx();
 		ConstructorEx c2 =new ConstructorEx(5);
-		ConstructorEx c3 =new ConstructorEx(2,5);
-		c1.function();
+		//*ConstructorEx c3 =new ConstructorEx(2,5);
+		//*c1.function();
 		c2.function();
-		c3.function();
+		//*c3.function();
 		
 		
 	}
