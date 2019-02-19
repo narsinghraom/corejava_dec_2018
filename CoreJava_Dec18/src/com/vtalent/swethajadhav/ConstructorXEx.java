c40a6d849fcfc787b5b16c2a7a05adc9a6ede1b9
jadhavswetha <swethajadhav.vtalent@gmail.com>
2019-02-19 11:40:59 +0530

    today's program

---
+++ CoreJava_Dec18/src/com/vtalent/swethajadhav/ConstructorXEx.java
@@ -0,0 +1,32 @@
+package com.vtalent.swethajadhav;
+
+public class ConstructorXEx {
+
+	int x,y;
+	ConstructorXEx(){
+		x = 5;
+		y = 7;
+	}
+	ConstructorXEx(int x){
+		this.x = this.x+ x;
+		y = y+x;	
+	}
+	ConstructorXEx(int x , int y){
+		this.x = this.x + x;
+		this.y = this.y + y;
+	}
+	public void function(){
+		System.out.println(x+y);
+		
+	}
+	public static void main(String[] args){
+		ConstructorXEx c1 = new ConstructorXEx();
+		ConstructorXEx c2 = new ConstructorXEx(5);
+		ConstructorXEx c3 = new ConstructorXEx(2,5);
+		c1.function();
+		c2.function();
+		c3.function();
+		
+	
+	}
+}
