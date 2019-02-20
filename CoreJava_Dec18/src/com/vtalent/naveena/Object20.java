220b039fa9adb131325305650d56f1a5a4318d65
Naveena09 <naveena.vtalent@gmail.com>
2019-02-20 12:20:19 +0530

    naveenaobject

---
+++ CoreJava_Dec18/src/com/vtalent/naveena/Object20.java
@@ -0,0 +1,18 @@
+package com.vtalent.naveena;
+
+public class Object20 {
+	Object20 o20=new Object20();
+	int k;
+	public void function(){
+		System.out.println(k);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object20 oe1=new Object20();
+		oe1.k=10;
+		//oe1.function();
+
+	}
+
+}
