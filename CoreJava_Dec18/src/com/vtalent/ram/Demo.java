fad6d98890a6e940b1c5de2b87ca23fef9450e27
ramcharan7 <ramcharanreddy.vtalent@gmail.com>
2019-02-20 11:46:56 +0530

    added variable k

--- CoreJava_Dec18/src/com/vtalent/ram/Demo.java
+++ CoreJava_Dec18/src/com/vtalent/ram/Demo.java
@@ -3,12 +3,13 @@ package com.vtalent.ram;
 class Demo {
 static Demo demo;
 Demo demo1;
-int i,j;
+int i,j,k;
 public static Demo function() {
 	demo=new Demo();
 	demo.demo1=new Demo();
 	demo.i=10;
 	demo.j=20;
+	demo.k=30;
 	demo.demo1.i=demo.i+demo.j;
 	demo.demo1.j=demo.i*demo.j;
 	return demo.demo1;
@@ -19,5 +20,6 @@ public static void main(String[] args) {
 	demo=function();
 	System.out.println(demo.i);
 	System.out.println(demo.j);
+	System.out.println(demo.k);
 }
 }
