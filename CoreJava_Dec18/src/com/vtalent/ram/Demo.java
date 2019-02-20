8ab2a7d696aa4a244edb88169733758b0c4e7161
ramcharan7 <ramcharanreddy.vtalent@gmail.com>
2019-02-20 11:22:55 +0530

    Demo

---
+++ CoreJava_Dec18/src/com/vtalent/ram/Demo.java
@@ -0,0 +1,23 @@
+package com.vtalent.ram;
+
+class Demo {
+static Demo demo;
+Demo demo1;
+int i,j;
+public static Demo function() {
+	demo=new Demo();
+	demo.demo1=new Demo();
+	demo.i=10;
+	demo.j=20;
+	demo.demo1.i=demo.i+demo.j;
+	demo.demo1.j=demo.i*demo.j;
+	return demo.demo1;
+	
+	
+}
+public static void main(String[] args) {
+	demo=function();
+	System.out.println(demo.i);
+	System.out.println(demo.j);
+}
+}
