7381595f11bf28c5b928cccc5561c64600a41d33
prashanthgopi123 <prashanth.vtalent@gmail.com>
2019-02-24 15:37:42 +0530

    codes

--- CoreJava_Dec18/src/com/vtalent/prashanthgopi/Palindrome.java
+++ CoreJava_Dec18/src/com/vtalent/prashanthgopi/Palindrome.java
@@ -1,21 +1,24 @@
 package com.vtalent.prashanthgopi;
 
+import java.util.Scanner;
+
 public class Palindrome {
 
-	 private static void mian(String args[])
-	    {
-			int num=121,r,sum=0;
-			int t=num;
-			while(num>0)
-			{
-				r=num%10;
-				num=num/10;
-				sum=sum*10+r;
-			}
-			if(t==sum)
-				System.out.println("palindrome");
-			else
-				System.out.println("not a palindrome");
-				
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = s.nextInt();
+		int r, sum = 0;
+		int t = num;
+		while (num > 0) {
+			r = num % 10;
+			num = num / 10;
+			sum = sum * 10 + r;
 		}
+		if (t == sum)
+			System.out.println("palindrome");
+		else
+			System.out.println("not a palindrome");
+
+	}
 }
