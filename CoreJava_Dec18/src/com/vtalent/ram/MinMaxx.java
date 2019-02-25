c15bca7530d0b689bd31d307d5ce8d3514a40777
ramcharan7 <ramcharanreddy.vtalent@gmail.com>
2019-02-25 12:17:06 +0530

    Max

---
+++ CoreJava_Dec18/src/com/vtalent/ram/MinMaxx.java
@@ -0,0 +1,18 @@
+package com.vtalent.ram;
+
+import java.util.Arrays;
+import java.util.Collections;
+public class MinMaxx {
+
+	
+	   public static void main(String[] args) {
+	      Integer[] numbers = { 8, 2, 7, 1, 4, 9, 5};
+	      int min = (int) Collections.min(Arrays.asList(numbers));
+	      int max = (int) Collections.max(Arrays.asList(numbers));
+	      
+	      System.out.println("Min number: " + min);
+	      System.out.println("Max number: " + max);
+	   }
+	}
+
+
