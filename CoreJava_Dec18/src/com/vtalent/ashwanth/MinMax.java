a0a1b633784a3653eae46c66a9abde8173b6b0ef
ashwanth9 <ashwanth.vtalent@gmail.com>
2019-02-25 10:13:17 +0530

    hlo

---
+++ CoreJava_Dec18/src/com/vtalent/ashwanth/MinMax.java
@@ -0,0 +1,20 @@
+package com.vtalent.ashwanth;
+import java.util.*;
+public class MinMax {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int a,b;
+	System.out.println("enter a:");
+	a=s.nextInt();
+	System.out.println("enter b:");
+	b=s.nextInt();
+	if(a>b)
+	{
+		System.out.println("a is big");
+	}
+	else
+	{
+		System.out.println("b is big");
+	}
+}
+}
