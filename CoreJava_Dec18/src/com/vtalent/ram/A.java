b594855eaaf6e57e264d22e689f7a4121c2d5f2c
ramcharan7 <ramcharanreddy.vtalent@gmail.com>
2019-02-25 12:01:49 +0530

    A

---
+++ CoreJava_Dec18/src/com/vtalent/ram/A.java
@@ -0,0 +1,14 @@
+package com.vtalent.ram;
+
+class A {
+int i,k;
+public String toString(){
+	return "ivalue="+i+" "+"kvalue="+k;
+}
+public static void main(String[] args) {
+	A a1=new A();
+	a1.i=10;
+	a1.k=20;
+	System.out.println(a1);
+}
+}
