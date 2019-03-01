d77068c411b969000d679f3250ca1a06f7f12358
chndureddy7 <chandureddy7.vtalent@gmail.com>
2019-03-01 09:21:03 +0530

    ample

--- CoreJava_Dec18/src/com/vtalent/naveena/Encapsulation_emp.java
+++ CoreJava_Dec18/src/com/vtalent/naveena/Encapsulation_emp.java
@@ -1,5 +1,4 @@
 package com.vtalent.naveena;
-
 public class Encapsulation_emp {
 public static void main(String[] args) {
 Test emp = new Test();
@@ -9,30 +8,20 @@ System.out.println("Id =" + emp.getEmpId());
 System.out.println("Salary =" + emp.getEmpSalary());
 }
 }
-
 class Test{
-	
 		// TODO Auto-generated method stub
 		private int empId;
 		private double empSalary;
-
 		public void setEmpId(int empId) {
 			this.empId = empId;
 		}
-
 		public void setEmpSalary(double EmpSalary) {
 			this.empSalary = empSalary;
 		}
-
 		public int getEmpId() {
 			return this.empId;
 		}
-
 		public double getEmpSalary() {
 			return this.empSalary;
 		}
-	
 	}
-
-
-
