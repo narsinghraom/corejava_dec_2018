51987addcd4eaae085bc6945b62b4039977bea19
sandeep61 <sandeep.vtalent@gmail.com>
2019-03-05 11:16:06 +0530

    Sa

--- CoreJava_Dec18/src/com/vtalent/sandeep/Inheretence.java
+++ CoreJava_Dec18/src/com/vtalent/sandeep/Inheretence.java
@@ -8,19 +8,19 @@ public class Inheretence {
 		
 	}
 
-public float function(float f1,float f2){
+    public int function(float f1,float f2)
+    {
 	this.f1=this .f1+f1;
 	this.f2=this.f2+f2;
-	return(int)(f1+f2);
-	
-}
+	return(int)(f1+f2);	
+   }
 
 public void function(){
-		System.out.println("f1+f2");
-		System.out.println("f2+f2");
+		System.out.println("f1"+f1);
+		System.out.println("f2"+f2);
 }	
 }
- class  one extends Inheretence{
+ class  One extends Inheretence{
 
 	long l1,l2;
 	float f1 ;
@@ -37,18 +37,7 @@ public void function(){
 		super.f1=super.f1 +f1;
 		return f1+f2;	
 }
- }
-class test{
-	public static void main(){
-		Inheretence d1= new Inheretence();
-		float f1=d1.function(2.5f,1.5f);
-		((Inheretence) d1).function((long)f1);
-		((Inheretence) d1).function(f1,f1);
-		((Inheretence) d1).function(0);
-		
-		
-	}
 	
-}
+ }
 
 
---
+++ CoreJava_Dec18/src/com/vtalent/sandeep/string.java
@@ -0,0 +1,5 @@
+package com.vtalent.sandeep;
+
+public class string {
+
+}
