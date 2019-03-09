63de0ba5f3a04636ba61ba633a3a422aaa7cb2bf
sandeep61 <sandeep.vtalent@gmail.com>
2019-03-09 16:02:40 +0530

    Sam

---
+++ CoreJava_Dec18/src/com/vtalent/sandeep/Singleton.java
@@ -0,0 +1,22 @@
+package com.vtalent.sandeep;
+
+public class Singleton {
+
+	
+    static Singleton SingletonRef=new Singleton();
+    Singleton(){
+	
+}
+    Public static Singleton Ref Get Instance();
+    {
+    	return SingletonRef Ref;
+    }
+}
+class test;
+
+  public Static void main(string[]args){
+
+	  Singleton xy = Singleton.getinstance();
+	  
+  }
+  
