73a3d77b3656d0b81ac6eb6e357801696da23078
Amreen Sulthana <amreen.vtalent@gmail.com>
2019-05-09 11:50:02 +0530

    debugexample

--- CoreJava_Dec18/src/com/vtalent/amreen/Basic1.java
+++ CoreJava_Dec18/src/com/vtalent/amreen/Basic1.java
@@ -1,28 +1,39 @@
 package com.vtalent.amreen;
 
+import java.util.Scanner;
+import java.util.*;
+/**
+ * 
+ * @author Admin
+ * 
+ */
 public class Basic1 {
-int i,j;
-static float f1,f2;
-public void functionOne()
-{
-	i=5;
-	j=7;
-}
-	public static int functionTwo(float f1)
-	{
-		Basic1.f1=f1+Basic1.f1;
-		f2=Basic1.f1+f2;
-		return (int)f1;
+	int i, j;
+	static float f1, f2;
+
+	public void functionOne() {
+		i = 5;
+		j = 7;
 	}
-	
-	
+
+	public static int functionTwo(float f1) {
+ Basic1.f1 = f1 + Basic1.f1;
+		f2 = Basic1.f1 + f2;
+		return (int) f1;
+	}
+
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		Basic1 obj=new Basic1();
+		Scanner sc=new Scanner(System.in);
+		Basic1 obj = new Basic1();
+		//obj.functionOne();
+		int k = functionTwo((float) obj.i);
 		obj.functionOne();
-		int k=functionTwo((float)obj.i);
-		System.out.println(obj.i+obj.j);
-		System.out.println(f1+f2);
-		
+		System.out.println(obj.i + obj.j);
+		System.out.println(f1 + f2);
 
 	}
 
