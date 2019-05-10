29b3a1d855420c74ab6fa6b4554023dfcbd2ef40
sapa17 <saichandparuchuri.vtalent@gmail.com>
2019-05-09 21:13:21 -0700

    sample

---
+++ CoreJava_Dec18/src/com/vtalent/saichand/doWhile.java
@@ -0,0 +1,18 @@
+package com.vtalent.saichand;
+
+/*
+ * do
+ {
+ statement(s);
+ } while(condition);
+ */
+public class doWhile {
+	public static void main(String args[]) {
+		int i = 10;
+		do {
+			System.out.println(i);
+			i--;
+		} while (i >= 1);
+	}
+
+}
