e732d9e226a6ddc1ed75eeba88c34d5604bfcfc8
sapa17 <saichandparuchuri.vtalent@gmail.com>
2019-05-14 09:47:50 -0700

    bla bla

--- CoreJava_Dec18/src/com/vtalent/saichand/whileloop.java
+++ CoreJava_Dec18/src/com/vtalent/saichand/whileloop.java
@@ -2,7 +2,7 @@ package com.vtalent.saichand;
 
 public class whileloop {
 	public static void main(String args[]) {
-		int i = 10, j = 5;
+		int i = 10;
 		while (i >= 1) {
 			System.out.println("the value of i is:" + i);
 			i--;
