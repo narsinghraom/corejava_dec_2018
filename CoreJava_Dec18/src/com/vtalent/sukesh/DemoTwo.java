758f7fa6152ab10f49cb3dd2a2487cf3852f5191
suekshjunapudi <sukeshjunapudi.vtalent@gmail.com>
2019-05-14 12:52:45 -0700

    my programs

---
+++ CoreJava_Dec18/src/com/vtalent/sukesh/DemoTwo.java
@@ -0,0 +1,25 @@
+package com.vtalent.sukesh;
+public class DemoTwo {
+	int i, j;
+	DemoTwo d;
+
+	public DemoTwo Function(DemoTwo d1) {
+		d = new DemoTwo();
+		d.d = new DemoTwo();
+		d.d.d = d1;
+		d.d.i = d1.i + d1.j;
+		d.d.j = d1.i * d1.j;
+		return d.d;
+	}
+
+	public static void main(String[] args) {
+		DemoTwo d1 = new DemoTwo();
+		d1.i = 7;
+		d1.j = 5;
+		DemoTwo d2 = d1.Function(d1);
+		System.out.println(d2.i + d2.j);
+		System.out.println(d1.i + d1.j);
+		System.out.println(d1.d.i + d1.d.j);
+	}
+
+}
