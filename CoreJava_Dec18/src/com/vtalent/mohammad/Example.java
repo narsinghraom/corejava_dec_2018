6f65aae94ba78b18b88b4d4df62143c4e0e665e8
sapa17 <saichandparuchuri.vtalent@gmail.com>
2019-05-15 11:06:24 -0700

    tc

--- CoreJava_Dec18/src/com/vtalent/mohammad/Example.java
+++ CoreJava_Dec18/src/com/vtalent/mohammad/Example.java
@@ -1,23 +1,23 @@
 package com.vtalent.mohammad;
 
 public class Example {
-	
+
 	double d1, d2;
-	
-	public long funtionOne(double d1,double d2) {
-		d1= d1+d2;
-		this.d2=this.d1+d2;
-		return (long)d1+d2;
+
+	public long funtionOne(double d1, double d2) {
+		d1 = d1 + d2;
+		this.d2 = this.d1 + d2;
+		return (long) ((long) d1 + d2);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Example exp=new Example();
+		Example exp = new Example();
 		exp.funtionOne(10.0, 15.0);
-		System.out.println(exp.d1+exp.d2);
+		System.out.println(exp.d1 + exp.d2);
 		System.out.println(exp);
-		
+
 	}
 
 }
