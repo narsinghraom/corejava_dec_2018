576d7211d5639df0ac5cbcc1fd284109d4e98d97
Amreen Sulthana <amreen.vtalent@gmail.com>
2019-05-19 09:02:37 +0530

    Employee Pgm using array

---
+++ CoreJava_Dec18/src/com/vtalent/amreen/Employee.java
@@ -0,0 +1,23 @@
+package com.vtalent.amreen;
+
+public class Employee {
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+	private int employeeId;
+	private double employeeSalary;
+
+}
