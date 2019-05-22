42872672c83096b6e7e93c39dc49ab86c66dbb77
kirankumar <kirankumar.vtalent@gmail.com>
2019-05-22 10:05:36 +0530

    added prime program

---
+++ CoreJava_Dec18/src/com/vtalent/kirankumar/EmployeeTest.java
@@ -0,0 +1,16 @@
+package com.vtalent.kirankumar;
+
+public class EmployeeTest {
+
+	/*
+	 * public static void main(String[] args) { EmployeeTest et = new
+	 * EmployeeTest(); EmployeeOperations eo = new EmployeeOperations(); Scanner
+	 * sc = new Scanner(System.in);
+	 * 
+	 * for(a==1) { System.out.println(""); int a = sc.nextInt();
+	 * System.out.println("1. Insert an employee data " + '\n' +
+	 * "2. update employee data" + '\n' + "3. delete employee data" + '\n' +
+	 * "4. search employee data" + '\n' + "5. printall employee data" + '\n' +
+	 * "6. exit"); eo. } }
+	 */
+}
