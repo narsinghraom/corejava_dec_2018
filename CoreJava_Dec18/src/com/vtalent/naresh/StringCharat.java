4ebc2e76ef5c6951ea479864e95f87d69e113052
naresh <kukutapunaresh.vtalent@gmai.com>
2019-05-30 11:37:59 +0530

    newly added

---
+++ CoreJava_Dec18/src/com/vtalent/naresh/StringCharat.java
@@ -0,0 +1,27 @@
+package com.vtalent.naresh;
+
+public class StringCharat {
+
+	String s1="Oliver";
+	static char [] ch;
+		public char Mychar(int index) {
+			 ch=new char[s1.length()];
+			int j=0;
+			for(int i=s1.length()-1;i>=0 ;i--) {
+				ch[j]=s1.charAt(i);
+				j++;
+			}
+			return ch[index];
+			}		
+		
+		public static void main(String[] args) {
+		StringCharat sc1=new StringCharat();
+		String s2=new String(ch);
+		System.out.println(s2);
+		
+		/*
+		 * char[] ch1= {'a','s','n'}; String s3=new String(ch);
+		 */
+		}
+		}
+
