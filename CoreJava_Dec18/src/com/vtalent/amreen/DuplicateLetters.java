67424d6b26ea380bda03c733f5e7535b8694f930
Amreen Sulthana <amreen.vtalent@gmail.com>
2019-06-04 14:41:02 +0530

    find DuplicateLetters

--- CoreJava_Dec18/src/com/vtalent/amreen/DuplicateLetters.java
+++ CoreJava_Dec18/src/com/vtalent/amreen/DuplicateLetters.java
@@ -3,40 +3,42 @@ package com.vtalent.amreen;
 import java.util.Scanner;
 
 public class DuplicateLetters {
+
 	public void findDuplicates() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter string");
 		String str = sc.nextLine();
 
 		char[] array = str.toCharArray();
-		int count = 0;
+		int count = 1;
 		int length = array.length;
-		char temp = '\0';
+		char[] temp = new char[length];
 
 		for (int i = 0; i < length; i++) {
-			if (array[i] != temp) {
-				for (int j = 0; j < length; j++) {
+
+			if (array[i] != temp[i]) {
+				for (int j = i + 1; j <= length - 1; j++) {
 
 					// char temp = 'x';
 					if (array[i] == array[j]) {
-						// temp = array[j];
 
 						count++;
 
 					}
-					temp = array[j];
+
+					temp[i] = array[i];
 
 				}
 				if (count > 1) {
+					if (array[i] > 1) {
+						System.out.println("character " + array[i]
+								+ " has repeated " + count + " times");
 
-					System.out.println("character " + array[i]
-							+ " has repeated " + count + " times");
-
+					}
+					count = 1;
 				}
-				count = 0;
 			}
 		}
-
 	}
 
 	public static void main(String[] args) {
