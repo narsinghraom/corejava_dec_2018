9459a13974ddb2cf00d029a23d133934a8683729
sanjanamergu5 <sanjanamergu.vtalent@gmail.com>
2019-06-06 20:27:22 +0530

    fy

--- CoreJava_Dec18/src/com/vtalent/sanjana/RwordsDel.java
+++ CoreJava_Dec18/src/com/vtalent/sanjana/RwordsDel.java
@@ -8,28 +8,39 @@ public class RwordsDel {
 		Scanner sc=new Scanner(System.in);
 		String s1=sc.nextLine();
 		int count=1;String s="";
+		String st="";
 		String[] s5=s1.split("\\s");
 		
-		for(int i=0;i<s5.length;i++)
+		for(int i=0;i<s5.length-1;i++)
 		{
 		for(int j=i+1;j<s5.length;j++)
 		{
 			if(s5[i].equals(s5[j]))
 			{
 				count++;
-				s=s+s5[j];
+				s=s5[i];
 			}
 			
 		}
-		}
-		if(count>=2)
+		
+		if(count>1)
 		{
+			if(st.contains(s))
+			{
+				count=1;
+			}
+		
+		    else 
+		    {
 			System.out.println(s+"-"+count);
+			count=1;
+			st=st+s;
+			}
 		}
-		else
-		{
-			System.out.println("No Repeated Words");
+		
+		
 		}
+		
 	}
 	}
 
