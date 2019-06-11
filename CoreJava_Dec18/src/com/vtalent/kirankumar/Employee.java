96f02520442656c80ee32c49cf0ea4719abd9f10
kirankumar <kirankumar.vtalent@gmail.com>
2019-06-11 20:16:11 +0530

    added

--- CoreJava_Dec18/src/com/vtalent/kirankumar/Employee.java
+++ CoreJava_Dec18/src/com/vtalent/kirankumar/Employee.java
@@ -1,23 +1,53 @@
 package com.vtalent.kirankumar;
 
-public class Employee {
-	private int EmployeeID;
-	private double EmployeeSalary;
-
-	public void setEmployeeID(int employeeID) {
-		EmployeeID = employeeID;
+import java.io.Serializable;
+public class Employee implements Serializable {
+	
+	private int employid;
+	private String employname;
+	private double employpackage;
+	private double employsalary;
+	private double employpf;
+	
+	
+	public void setEmployId(int employid) {
+		this.employid=employid;
 	}
-
-	public void setEmployeeSalary(double employeeSalary) {
-		EmployeeSalary = employeeSalary;
+	
+	public void setEmployname(String employname) {
+		this.employname=employname;
 	}
-
-	public int getEmployeeID() {
-		return EmployeeID;
+	
+	public void setEmployPackage(double employpackage) {
+		this.employpackage=employpackage;
 	}
-
-	public double getEmployeeSalary() {
-		return EmployeeSalary;
+	public void setEmploySalary(double employsalary) {
+		this.employsalary=employsalary;
 	}
-
-}
+	
+	public void setEmployPf(double employpf) {
+		this.employpf=employpf;
+	}
+	
+	public int getEmployId() {
+		return employid;
+	}
+		public double getEmployPackage() {
+			return employpackage;
+		}
+		
+		public String getEmployname() {
+			return employname;
+		}
+		
+		public double getEmploySalary() {
+			double a=employpackage/12;	//calculating monthly salary
+			employpf =(a*2)/100;	// calculating pf
+			employsalary=a-employpf;
+			return employsalary;
+			//System.out.print("Monthly Salary- "+employsalary+"  "+" Provident Fund(2%)- "+employpf);
+		}
+		public double getEmployPf() {
+			return employpf;
+		}
+	}
