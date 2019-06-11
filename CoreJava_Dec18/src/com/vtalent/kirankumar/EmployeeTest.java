cb124b441672b5f29ff34f7584d6fec247e46089
kirankumar <kirankumar.vtalent@gmail.com>
2019-06-11 19:33:31 +0530

    added

--- CoreJava_Dec18/src/com/vtalent/kirankumar/EmployeeTest.java
+++ CoreJava_Dec18/src/com/vtalent/kirankumar/EmployeeTest.java
@@ -2,15 +2,18 @@ package com.vtalent.kirankumar;
 
 public class EmployeeTest {
 
-	/*
-	 * public static void main(String[] args) { EmployeeTest et = new
-	 * EmployeeTest(); EmployeeOperations eo = new EmployeeOperations(); Scanner
-	 * sc = new Scanner(System.in);
-	 * 
-	 * for(a==1) { System.out.println(""); int a = sc.nextInt();
-	 * System.out.println("1. Insert an employee data " + '\n' +
-	 * "2. update employee data" + '\n' + "3. delete employee data" + '\n' +
-	 * "4. search employee data" + '\n' + "5. printall employee data" + '\n' +
-	 * "6. exit"); eo. } }
-	 */
+	
+	  public static void main(String[] args) { 
+		  EmployeeTest et = new EmployeeTest(); 
+		  EmployeeOperations eo = new EmployeeOperations(); 
+		  Scanner sc = new Scanner(System.in);
+	  
+	  for(a==1) { 
+		  System.out.println(""); 
+		  int a = sc.nextInt();
+	  System.out.println("1. Insert an employee data " + '\n' +
+	  "2. update employee data" + '\n' + "3. delete employee data" + '\n' +
+	  "4. search employee data" + '\n' + "5. printall employee data" + '\n' +
+	  "6. exit"); eo. } }
+	 
 }
