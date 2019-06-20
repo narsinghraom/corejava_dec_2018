356eed48b4570c15a211eb2c8c1ee8500e7a0ce7
saritha <saritha.vtalent@gmail.com>
2019-06-20 10:10:33 +0530

    a

---
+++ CoreJava_Dec18/src/com/vtalent/saritha/GetALoanException.java
@@ -0,0 +1,9 @@
+package com.vtalent.saritha;
+
+public class GetALoanException extends Exception {
+
+	public String toString() {
+		return "salary should be grater than 25,000.not eligible for loan";
+
+	}
+}
