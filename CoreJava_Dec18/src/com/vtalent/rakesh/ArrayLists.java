f235a8a2f83feaf893d9f9896b470689dcf6c860
rakesh <rakeshproddutur.vtalent@gmail.com>
2019-06-27 13:57:06 +0530

    added

--- CoreJava_Dec18/src/com/vtalent/rakesh/ArrayLists.java
+++ CoreJava_Dec18/src/com/vtalent/rakesh/ArrayLists.java
@@ -17,34 +17,37 @@ public class ArrayLists {
 
 	public static void main(String[] args) {
 
+		ArrayList<String> al = new ArrayList<>();
+		al.add("rakesh");
+		al.add(1, "q");
+		System.out.println(al);
+
+		al.add(2, "q");
+		// System.out.println(al);
 		ArrayLists q = new ArrayLists();
 		q.setQwerty("rakesh");
 		a.add(q);
-		b.add(q);
-		a.addAll(b);
-		System.out.println(a);
-		for ( q : a) {
-			System.out.println();
-		}
+		/*
+		 * b.add(q); a.addAll(b);
+		 */
 		// System.out.println(a);
-		// System.out.println(a.size());
-		/*
-		 * a.addAll(b);
-		 * 
-		 * // System.out.println(a); // System.out.println(a.size());
-		 * System.out.println(a.indexOf(123)); System.out.println(a.get(0));
-		 */
-		/*
-		 * Iterator<String> itr = a.iterator(); while (itr.hasNext()) { String s =
-		 * (String) itr.next(); System.out.println(s);
-		 * 
-		 * }
-		 */
-		/*
-		 * for (int s : a) { System.out.println(s);
-		 * 
-		 * }
-		 */
+
+		System.out.println(a);
+		System.out.println(a.size());
+
+		System.out.println(a.addAll(b));
+		System.out.println(a);
+		System.out.println(a.contains(q));
+		System.out.println(a.isEmpty());
+		// System.out.println(a);
+		System.out.println(a.size());
+		System.out.println(a.indexOf(123));
+		System.out.println(a.get(0));
+		System.out.println(a.clone());
+
+		for (ArrayLists z : a) {
+			System.out.println(z.getQwerty());
+		}
 
 	}
 
