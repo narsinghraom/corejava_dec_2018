598bf92f6241460c654680907bf70457c6c46dd4
lokesh194 <54094091+lokesh194@users.noreply.github.com>
2019-08-21 22:00:48 +0530

    newly basic project

---
+++ CoreJava_Dec18/src/com/vtalent/java/lokesh/Sample.java
@@ -0,0 +1,21 @@
+package com.vtalent.java.lokesh;
+
+public class Sample {
+int x,y;
+static float f1;
+public void function() {
+	x=5;
+	y=x+5;
+}
+public static float function(int x) {
+	f1=x+0.5f;
+	return f1;
+}
+public static void main(String[] args) {
+	Sample sample =new Sample();
+	sample.function();
+	f1=function (sample.y);
+	System.out.println(sample.x+sample.y);
+	System.out.println(f1);
+}
+}
