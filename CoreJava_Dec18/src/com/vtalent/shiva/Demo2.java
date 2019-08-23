043162ddbc3bbeb49f52a69671840a6744827a55
shiva <shivavtalent62@gmail.com>
2019-08-23 19:13:10 +0530

    sample

--- CoreJava_Dec18/src/com/vtalent/shiva/Demo2.java
+++ CoreJava_Dec18/src/com/vtalent/shiva/Demo2.java
@@ -2,13 +2,24 @@ package com.vtalent.shiva;
 
 public class Demo2 {
 
-	public Demo2() {
-		// TODO Auto-generated constructor stub
+	int x,y;
+	static float f1,f2;
+	public void function(int x,int y) {
+	this.x=this.x+x;
+	this.y=this.y+y;
+	
+	}
+	public static void function(float f1) {
+		Demo2.f1=f1+f2;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+     Demo2 d2=new Demo2();
+     d2.function(2, 3);
+     function(1.5f);
+     System.out.println(d2.x+d2.y);
+     System.out.println(f1+f2);
 	}
 
 }
