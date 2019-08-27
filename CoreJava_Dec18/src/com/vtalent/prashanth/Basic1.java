b5897975b608c00acec814b61a5fe27fbb324f63
Prashanth Reddy <prashanthreddy.vtalent@gmail.com>
2019-08-27 10:00:05 +0530

    Basic1.java DemoTwo1.java ThisKeyword.java

---
+++ CoreJava_Dec18/src/com/vtalent/prashanth/Basic1.java
@@ -0,0 +1,28 @@
+package com.vtalent.prashanth;
+
+public class Basic1 {
+	int i,j;
+	static float f1,f2;
+	
+	public void functionOne() {
+	i=5;
+	j=7;
+	}
+	
+	public static int functionTwo(float f1) {
+		Basic1.f1 = f1 + Basic1.f1;
+		f2 = Basic1.f1 + f2;
+		return (int) f1;
+	}
+
+	public static void main(String[] args) {
+		Basic1 obj = new Basic1();
+		obj.functionOne();
+		int k = functionTwo((float) obj.i);
+		obj.functionOne();
+		System.out.println(obj.i + obj.j);
+		System.out.println(f1 + f2);
+		
+	}
+
+}
