03bc268b18981ee5d8f7b0d0f7a47df3420516bc
Prashanth Reddy <prashanthreddy.vtalent@gmail.com>
2019-08-28 11:39:08 +0530

    Abc.java , ThisEx.java

---
+++ CoreJava_Dec18/src/com/vtalent/prashanth/ThisEx.java
@@ -0,0 +1,28 @@
+package com.vtalent.prashanth;
+
+public class ThisEx {
+	int i,j;
+	ThisEx(){
+		i=5;
+		j=7;
+	}
+	ThisEx(int i){
+		this.i = this.i + i;
+		j = j + this.i;
+	}
+	ThisEx(int i,int j){
+		this.i = this.i + i;
+		this.j = this.j * j;
+	}
+
+	public static void main(String[] args) {
+		ThisEx e1 = new ThisEx();
+		ThisEx e2 = new ThisEx(2);
+		ThisEx e3 = new ThisEx(3,5);
+		System.out.println(e1.i + e1.j);
+		System.out.println(e2.i + e2.j);
+		System.out.println(e3.i + e3.j);
+
+	}
+
+}
