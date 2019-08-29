4f7b4c7e68c1548e2e649979d21620508e73ff6a
srikarreddy <srikarreddy.vtalent@gmail.com>
2019-08-29 11:54:19 +0530

    array ex and bubble sorting

---
+++ CoreJava_Dec18/src/com/vtalent/java/srikarreddy/BubbleSort.java
@@ -0,0 +1,22 @@
+package com.vtalent.java.srikarreddy;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[]a= {4,2,1,5,3};
+		for(int i=0;i<a.length;i++) {
+			
+		 for(int j=0;j<a.length;j++) {
+			 if(a[i]<a[j]) {
+				 int temp=a[i];
+				 a[i]=a[j];
+				 a[j]=temp;}
+			 }}
+				System.out.println("printing sorted list.....");
+				for(int i =0;i<a.length;i++) {  
+			  
+				System.out.println(a[i]);
+		 }}}
+	  
+ 
+	 
+ 
