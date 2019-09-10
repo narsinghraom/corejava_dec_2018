55aec55759f077ccf7fc07c7cbaed02524883097
shiva <shivavtalent62@gmail.com>
2019-09-10 12:49:17 +0530

    hi

---
+++ CoreJava_Dec18/src/com/vtalent/shiva/PolyTwo.java
@@ -0,0 +1,16 @@
+package com.vtalent.shiva;
+
+public class PolyTwo extends Polyone {
+	double a;
+	public int function(double a) {
+		this.a=this.a+a;
+		b=b+a;
+		return (int)(a+b);
+		
+	}
+public int function (double a,double b) {
+	this.a=this.a+a;
+	b=b+a;
+	return (int)(a+b);
+   }
+}
---
+++ CoreJava_Dec18/src/com/vtalent/shiva/Polyone.java
@@ -0,0 +1,18 @@
+package com.vtalent.shiva;
+
+public class Polyone {
+	double a,b;
+	static int x,y;
+	public int function (double a) {
+		this.a=this.a+a;
+		b=b+a;
+		return(int)a;
+		
+	}
+   public double function(int x,int y) {
+	   Polyone.x=Polyone.x+x;
+	   Polyone.y=Polyone.y+y;
+	   return(double)(x+y);
+	   
+   }
+}
