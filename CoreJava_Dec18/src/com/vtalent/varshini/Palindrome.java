e30cd3c19644f6cf1f661b7da1e72ae2f687dc8c
varshinireddy <varshinigopi1886@gmail.com>
2019-09-27 22:58:30 +0530

    string methods and palindrome

---
+++ CoreJava_Dec18/src/com/vtalent/varshini/Palindrome.java
@@ -0,0 +1,21 @@
+package com.vtalent.varshini;
+import java.util.*;
+
+public class Palindrome {
+	 
+	   public static void main(String args[])  
+	   {  
+	      String original, reverse = ""; 
+	      Scanner sc = new Scanner(System.in);   
+	      System.out.println("Enter a string/number to check if it is a palindrome");  
+	      original = sc.nextLine();   
+	      int length = original.length();   
+	      for ( int i = length - 1; i >= 0; i-- )  
+	         reverse = reverse + original.charAt(i);  
+	      if (original.equals(reverse))  
+	         System.out.println("Entered string/number is a palindrome.");  
+	      else  
+	         System.out.println("Entered string/number isn't a palindrome.");   
+	   }  
+	}  
+
--- CoreJava_Dec18/src/com/vtalent/varshini/Stringmethods.java
+++ CoreJava_Dec18/src/com/vtalent/varshini/Stringmethods.java
@@ -1,5 +1,7 @@
 package com.vtalent.varshini;
 
+import java.util.Scanner;
+
 public class Stringmethods {
 	
 		public static void main(String[] args) {
@@ -8,14 +10,14 @@ public class Stringmethods {
 			String s2= "Java";
 			String s3 = "  Hello Java  ";
 			
-			System.out.println("Char at index 2(third position): " + String.charAt(2));
+			System.out.println("Char at index 2 " + String.charAt(2));
 			System.out.println("After Concat: "+ String.concat("-Enjoy-"));
 			System.out.println("Checking equals ignoring case: " +s2.equalsIgnoreCase(s1));
 			System.out.println("Checking equals with case: " +s2.equals(s1));
 			System.out.println("Checking Length: "+ String.length());
 			System.out.println("Replace function: "+ String.replace("fun", "easy"));
-			System.out.println("SubString of targetString: "+ String.substring(8));
-			System.out.println("SubString of targetString: "+ String.substring(8, 12));
+			System.out.println("SubString of string: "+ String.substring(8));
+			System.out.println("SubString of String: "+ String.substring(8, 12));
 			System.out.println("Converting to lower case: "+ String.toLowerCase());
 			System.out.println("Converting to upper case: "+ String.toUpperCase());
 			System.out.println("Triming string: " + s3.trim());
@@ -25,9 +27,18 @@ public class Stringmethods {
 			char [] charArray = s2.toCharArray();
 			System.out.println("Size of char array: " + charArray.length);
 			System.out.println("Printing last element of array: " + charArray[3]);
-
 		}
-
+			
+	/*
+	 * public void concat(){
+	 * 
+	 * System.out.println("Enter Two Strings:"); 
+	 * Scanner sc = new Scanner(System.in); 
+	 * String S1 = sc.nextLine();
+	 *  String S2 = sc.nextLine();
+	 * String S3 = S1 + S2; S3 = S3.toUpperCase();
+	 *  System.out.println(S3); }
+	 */
 	}
 
 
