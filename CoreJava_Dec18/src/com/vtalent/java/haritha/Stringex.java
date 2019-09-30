e4607ab8b365185647512515a4051cdb651ce3d1
harithavaltheru <haritha.vtalent@gmail.com>
2019-09-30 12:14:45 +0530

    Employeemain Palindrome.java

--- CoreJava_Dec18/src/com/vtalent/java/haritha/Stringex.java
+++ CoreJava_Dec18/src/com/vtalent/java/haritha/Stringex.java
@@ -7,29 +7,21 @@ public class Stringex {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter string");
 		String s=sc.next();
-		char c=s.charAt(5);
+		char c=s.charAt(4);
 		System.out.println("character at index:" +c);
 		int r=s.codePointAt(1);
 		System.out.println("character(unicode point)="+r);
-		int r1=s.codePointBefore(2);
+		int r1=s.codePointBefore(4);
 		System.out.println("character(unicode point)="+r1);
-		int cd=s.codePointCount(0,3);
+		int cd=s.codePointCount(1,2);
 		System.out.println("codepointcount="+cd);
 		System.out.println("enter string1");
 		String s1=sc.next();
 		System.out.println("enter string2");
 		String s2=sc.next();
-		int r3=s1.compareTo(s2);
-		
-		  if(r3>0) { 
-			  System.out.println("string1 is greater than string2");
-			  } else
-		  if(r3==0){
-			  System.out.println("string1 is equal to string2"); 
-			  } else {
-		  System.out.println("string1 is lesser than string2"); }
-		 
-		
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.offsetByCodePoints(3,4));
+		System.out.println(s1.equals(s2));
 		}
 	
 	
