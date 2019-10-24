98fa7bd6100f851e3ee2715573d60b828146ac0b
shiva <shivavtalent62@gmail.com>
2019-10-24 12:36:10 +0530

    sample

---
+++ CoreJava_Dec18/src/com/vtalent/shiva/CktExceptions.java
@@ -0,0 +1,10 @@
+package com.vtalent.shiva;
+
+import java.io.IOException;
+
+public class CktExceptions {
+	public static void rainEx()throws IOException{
+	
+	}
+
+}
---
+++ CoreJava_Dec18/src/com/vtalent/shiva/Cricket.java
@@ -0,0 +1,17 @@
+package com.vtalent.shiva;
+
+import java.util.Scanner;
+public class Cricket {
+	public static void main(String[] args) {
+		FirstInnings c=new FirstInnings();
+		c.rain();
+		
+		
+		
+			
+
+        
+	     }
+
+	
+	}
