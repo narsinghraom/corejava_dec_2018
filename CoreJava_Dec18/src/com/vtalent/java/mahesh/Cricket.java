09d344614f33ba6ab4c9e3299cb9f9aa220390d5
MaheshKamuju <54261609+MaheshKamuju@users.noreply.github.com>
2019-10-26 15:07:35 +0530

    cricket task completed along all exceptions.

--- CoreJava_Dec18/src/com/vtalent/java/mahesh/Cricket.java
+++ CoreJava_Dec18/src/com/vtalent/java/mahesh/Cricket.java
@@ -2,12 +2,15 @@ package com.vtalent.java.mahesh;
 
 import java.util.Scanner;
 
+import java.util.Random;
+
 public class Cricket {
 	static Scanner in=new Scanner(System.in);
-	static String a;
-	static String b;
-	static float e;
-	static float h;
+	static String a,b;
+	static float e,h;
+	static int z;
+	Random r=new Random();
+	static boolean innings=false;
 	static boolean fortyoversflag=false;
 	 static class Over extends Exception{
 		Over(String s){
@@ -42,6 +45,34 @@ public class Cricket {
 			 throw new Over("Runs Should not exceed 6 more than from first team score");
 		 }
 	 }
+	 public void rain() throws Over{
+		 Scanner in=new Scanner(System.in);
+			System.out.println("Is It Raining yes/no:");
+			String n=in.next();
+			if(n.equals("yes")){
+			System.out.println("Is It Raining for 2 hours yes/no:");
+			String p=in.next();
+				if(p.equals("yes")){
+					System.out.println("Is It Raining for 3 hours yes/no:");
+					String k=in.next();
+					if(k.equals("no")){
+						fortyoversflag=true;
+						if(innings==false)
+						{
+						FirstInnings();
+						}else if(innings==true)
+						{
+							e=(int)((75*e)/100);
+							System.out.println("First teams score due to rain is:"+e);
+							SecondInnings();
+						}
+					}else if(k.equals("Yes")){
+						System.out.println("Match is Abundant due to heavy rain");
+						System.exit(0);
+					}
+				}
+			}
+	 }
 	public void FirstInnings() throws Over{
 		System.out.println("enter the first team name");
 		a=in.next();
@@ -58,9 +89,17 @@ public class Cricket {
 		System.out.println("Enter the run rate:");
 		float d=in.nextFloat();
 		e=(int) (f*d);
-		System.out.println("The score of\t"+ a +"\tteam is:"+e);
+		if(f<50){
+			z=10;
+		}
+		else if(f==50){
+			 z=r.nextInt(9);
+		}
+		System.out.println("The score of\t"+ a +"\tteam is:"+(int)e+"/"+z);
+		innings=true;
 	}
 	public void SecondInnings() throws Over{
+		rain();
 		System.out.println("Second Innings by"+"  "+b);
 		System.out.println("Enter the number of overs:");
 		float x=in.nextFloat();
@@ -72,41 +111,23 @@ public class Cricket {
 	System.out.println("Run rate should not exceed more than 2 decimal points of"+(e/x));
 	float g=in.nextFloat();
 	h=(int) (x*g);
-	System.out.println("The score of\t"+ b +"\tteam is:"+h);
+	if(x==50&&h<e){
+		z=10;
+	}
+	else if(x<50&&(h<(e+6)&&h>e)){
+		z=r.nextInt(9);
+	}
+	System.out.println("The score of\t"+ b +"\tteam is:"+(int)h+"/"+z);
 	}
 	public static void main(String args[]) throws Over{
 		int l=0;		
 		Cricket c=new Cricket();
-		 Scanner in=new Scanner(System.in);
-			System.out.println("Is It Raining yes/no:");
-			String n=in.next();
-			if(n.equals("yes")){
-			System.out.println("Is It Raining for 2 hours yes/no:");
-			String p=in.next();
-				if(p.equals("yes")){
-					System.out.println("Is It Raining for 3 hours yes/no:");
-					String k=in.next();
-					if(k.equals("no")){
-						fortyoversflag=true;
-						c.FirstInnings();
-						l++;
-						if(l==1){
-							c.SecondInnings();
-						}
-					}else if(k.equals("Yes")){
-						System.out.println("Match is Abundant due to heavy rain");
-						System.exit(0);
-					}
-				}
-			}
-		c.FirstInnings();
-		l++;
-		if(l==1){
-		c.SecondInnings();
-		}
+			c.rain();
+			c.FirstInnings();
+				c.SecondInnings();
 		if(e>h){
 			System.out.println(a+"  "+"Won the match by"+" "+(e-h)+" "+"runs");
-		}else if(h>e&&h<(e+6)){
+		}else if(h>e&&h<=(e+6)){
 			System.out.println(b+"  "+"Won the match by"+" "+(h-e)+" "+"runs");
 		}
 			else if(h>(e+6)){
