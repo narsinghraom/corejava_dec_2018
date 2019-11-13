5439a042f21c8bf24896818d835135360b6991ba
shivag62 <shivavtalent62@gmail.com>
2019-11-13 18:06:55 +0530

    update

--- CoreJava_Dec18/src/com/vtalent/shiva/DBOperation.java
+++ CoreJava_Dec18/src/com/vtalent/shiva/DBOperation.java
@@ -15,7 +15,7 @@ public class DBOperation {
 	
 	public int insertDataWithStatement(EmployeeBean2 employee) {
 		int result = 0;
-		String query = "insert into Employee_table values("+employee.getEmployeeId()+",'"+employee.getEmployeeName()+"',"+employee.getEmployeeSalary()+","+employee.getEmployeeMobileNo()+")";
+		String query = "insert into employee_table values("+employee.getEmployeeId()+",'"+employee.getEmployeeName()+"',"+employee.getEmployeeSalary()+","+employee.getEmployeeMobileNo()+")";
 		try {
 			statement = connection.createStatement();
 			result =statement.executeUpdate(query);
@@ -27,7 +27,7 @@ public class DBOperation {
 	
 	public int deleteDataWithStatement(EmployeeBean2 employee) {
 		int result1=0;
-		String query = "delete from employee_table where employeeName='"+employee.getEmployeeName()+"'";
+		String query = "delete from employee_table  where employeeName='"+employee.getEmployeeName()+"'";
 		try {
 			statement = connection.createStatement();
 			result1=statement.executeUpdate(query);
@@ -36,6 +36,20 @@ public class DBOperation {
 		}
 		return result1;
 		
+	}
+	
+	public int updteDataWithStatement(EmployeeBean2 employee) {
+		int result2=0;
+		String query = "update  from employee_table set employeeName='"+employee.getEmployeeName()+"' WHERE employeeId='"+employee.getEmployeeId();
+		try {
+			statement = connection.createStatement();
+			result2 = statement.executeUpdate(query);
+			} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result2;
+	
 	}
 	
 	public static void main(String[] args) {
@@ -60,11 +74,15 @@ public class DBOperation {
 			System.out.println("Employee data insertion unsuccessfull");
 			
 		}
-		
+		System.out.println("do you want to delete yes or no");
+		String s=sc.next();
+		if(s.equals("yes")) {
 		System.out.println("Enter the Employee Name to be Delete: ");
 		employee.setEmployeeName(sc.next());
 		DBOperation dbOperation1 = new DBOperation();
 		int result1 = dbOperation1.deleteDataWithStatement(employee);
+
+		
 		if(result>0) {
 			System.out.println("Employee data has been Deleted successfully");
 			
@@ -72,8 +90,24 @@ public class DBOperation {
 			System.out.println("Employee data Deletion unsuccessfull");
 			
 		}
+		}
+		
+		System.out.println("do you want to update yes or no"); 
+		String s1=sc.next();
+		DBOperation dbOperation2 = new DBOperation();
+		if(s1.equals("yes")) {
+		System.out.println("Enter the Employee Name to be Updated: ");
+		employee.setEmployeeName(sc.next()); 
+		System.out.println("Enter the Id to assign the given name");
+		employee.setEmployeeId(sc.nextInt());
 		
 		
+		int result2 = dbOperation2.updteDataWithStatement(employee);
+		if(result>0) {
+			System.out.println("Employee data  updated");
+		}else {
+			System.out.println("not yet updated properly");
+		}
 	}
-
+	}
 }
