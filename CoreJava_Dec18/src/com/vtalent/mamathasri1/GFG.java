b22d4283f289479d191c40ce5d53a34c74348a87
mamathasri <mamathasri.vtalent@gmail.com>
2019-11-14 12:37:03 +0530

    new

--- CoreJava_Dec18/src/com/vtalent/mamathasri1/GFG.java
+++ CoreJava_Dec18/src/com/vtalent/mamathasri1/GFG.java
@@ -1,17 +1,30 @@
 package com.vtalent.mamathasri1;
 
-import java.util.ArrayList;
+import java.io.*;
 import java.util.*;
-import java.util.AbstractCollection;
-public class GFG {
-	public static void main(String args[])
-	{
-	AbstractCollection<Object> abs = new ArrayList<Object>(); 
-	abs.add("welcome");
-	abs.add("to");
-	abs.add("java");
-	abs.add("vtalent");
-	System.out.println("Abstact collection"+abs);
+class GFG
+{
+	public static void main(String args[]) {
+		ArrayList<String> a1=new ArrayList<String>();
+		a1.add("practice. to arraytlist");
+		a1.add("Quiz. geeks for geeks.org");
+		a1.add("code. geeks for geeks.org");
+		a1.add("contribute. for geeks for geks");
+	System.out.println("ArrayList Elements are");
+	Iterator it =a1.iterator();
+	while (it.hasNext()) {
+	      System.out.println(it.next());
+	      Vector<String> v=new Vector<String>();
+	      v.addElement("practice");
+	      v.addElement("Quiz");
+	      v.addElement("code");
+          System.out.println("/n vector elements are");
+          Enumeration e = v.elements();
+          
+     while(e.hasMoreElements()) {
+           System.out.println(e.nextElement());
+     }
+	}
+	}
+}
 
-}
-}
