bbc05ce29ce547b6abb47e00b19753c3f6aed226
Jaya <vijaya.vtalent@gmail.com>
2019-12-04 11:31:04 +0530

    HELLO

---
+++ CoreJava_Dec18/src/com/vtalent/vijaya/basic/Sample.java
@@ -0,0 +1,17 @@
+package com.vtalent.vijaya.basic;
+
+public class Sample {
+	int a,b;
+	static int c;
+	public static void main(String[] args) {
+		c=10;
+		Sample s1=new Sample();
+		s1.function(c);
+		System.out.println(s1.a+s1.b+c);
+	}
+	public void function(int c) {
+		a=c;
+		b=c+5;
+	}
+
+}
