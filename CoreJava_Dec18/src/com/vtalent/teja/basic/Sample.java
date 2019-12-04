8143c45f99aee4003b42a42af6b797f1c98b21c1
teja <teja.vtalent@gmail.com>
2019-12-04 11:49:31 +0530

    testing

--- CoreJava_Dec18/src/com/vtalent/teja/basic/Sample.java
+++ CoreJava_Dec18/src/com/vtalent/teja/basic/Sample.java
@@ -1,5 +1,16 @@
 package com.vtalent.teja.basic;
 
 public class Sample {
-
+int a,b;
+static int c;
+public static void main(String[] args) {
+	c=10;
+	Sample s1=new Sample();
+	s1.function(c);
+	System.out.println(s1.a+s1.b+c);
+}
+public void function(int c) {
+	a=c;
+	b=c+5;
+}
 }
