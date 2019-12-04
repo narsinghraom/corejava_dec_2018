dab4d547ceb9b9c9745a7a2061b63b6907aacb5c
sravanvakati <sravanvakati.vtalent@gmail.com>
2019-12-04 12:07:09 +0530

    test me*

--- CoreJava_Dec18/src/com/vtalent/sravan/Sample1.java
+++ CoreJava_Dec18/src/com/vtalent/sravan/Sample1.java
@@ -1,5 +1,21 @@
 package com.vtalent.sravan;
 
 public class Sample1 {
+	int a, b;
+	static int c;
+	
+	
+	
+	public static void main(String[] args) {
+		c = 10;
+		Sample1 s1= new Sample1();
+		s1.function(c);
+		System.out.println(s1.a + s1.b + c);
+	}
+	public void function(int c) {
+		a = c;
+		b = c + 5;
+	}
+	
 
 }
