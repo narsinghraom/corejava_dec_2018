8b6db745ba6873676d47ce9b9f52c2a8545b90a3
sathyamk1 <satyanarayana.vtalent@gmail.com>
2019-12-04 11:21:31 +0530

    sample test

---
+++ CoreJava_Dec18/src/com/vtalent/sathyam/basics/Palindrome.java
@@ -0,0 +1,18 @@
+package com.vtalent.sathyam.basics;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		int num = 121, reversedInteger = 0, remainder, originalInteger;
+		originalInteger = num;
+		while (num != 0) {
+			remainder = num % 10;
+			reversedInteger = reversedInteger * 10 + remainder;
+			num /= 10;
+		}
+		if (originalInteger == reversedInteger)
+			System.out.println(originalInteger + " is a Palindrome.");
+		else
+			System.out.println(originalInteger + " is not a Palindrome.");
+	}
+
+}
