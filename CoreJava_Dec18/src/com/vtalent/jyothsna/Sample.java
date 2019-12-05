ac5ff095537ac4581179a2610c2d98b6a37a7277
Jyothsna <jyothsna.vtalent@gmail.com>
2019-12-05 21:52:31 +0530

    Code changes

--- CoreJava_Dec18/src/com/vtalent/jyothsna/Sample.java
+++ CoreJava_Dec18/src/com/vtalent/jyothsna/Sample.java
@@ -1,18 +1,21 @@
 package com.vtalent.jyothsna;
 
 public class Sample {
-	int a,b;
-	static int c;	
+	int a, b;
+	static int c;
+
 	public static void main(String[] args) {
-		c=10;
-		Sample s1= new Sample();
+		c = 10;
+		Sample s1 = new Sample();
 		s1.function(c);
-		System.out.println(s1.a+s1.b+c);
+		System.out.println(s1.a + s1.b + c);
 	}
-public void function(int c) {	
-	a=c;
-	b=c+5;
-	c=a+b;
+
+	public void function(int c) {
+		a = c;
+		b = c + 5;
+		c = a + b;
+		
+		System.out.println(c);
+	}
 }
-}
-				
