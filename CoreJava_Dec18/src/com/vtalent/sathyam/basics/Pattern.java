aa8aef7c39facf58c60f9f1eaf7c9e992f9b5721
sathyamk1 <satyanarayana.vtalent@gmail.com>
2019-12-05 09:18:59 +0530

    test pattern

--- CoreJava_Dec18/src/com/vtalent/sathyam/basics/Pattern.java
+++ CoreJava_Dec18/src/com/vtalent/sathyam/basics/Pattern.java
@@ -5,7 +5,7 @@ public class Pattern {
 	        int rows = 5;
 	        for(int i = 1; i <= rows; ++i) {
 	            for(int j = 1; j <= i; ++j) {
-	                System.out.print(" * ");
+	                System.out.print("* ");
 	            }
 	            System.out.println();
 	        }
