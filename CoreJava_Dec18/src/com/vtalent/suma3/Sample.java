500ea8f2158fbcc0c23c10b1d68e88d80fcbf3be
sumathi92 <sumathi.vtalent@gmail.com>
2019-12-06 10:11:20 +0530

    test

--- CoreJava_Dec18/src/com/vtalent/suma3/Sample.java
+++ CoreJava_Dec18/src/com/vtalent/suma3/Sample.java
@@ -3,17 +3,17 @@ package com.vtalent.suma3;
 class Sample {
 	int a, b;
 	static int c;
+	
+	public void function(int c) {
+		a = c;
+		b = c+5;
+	}
 
 	public static void main(String[] args) {
 		c = 10;
 		Sample s1 = new Sample();
-		s1.funtion(c);
+		s1.function(c);
 		System.out.println(s1.a + s1.b + c);
-	}
-
-	public void funtion(int c) {
-		a = c;
-		b = c + 5;
 	}
 
 }
