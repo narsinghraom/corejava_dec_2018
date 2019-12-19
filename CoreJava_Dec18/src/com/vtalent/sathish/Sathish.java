9f26a1c4f0d039e04d9016feae62315835fd6d22
sathish <sathish.vtalent@gmail.com>
2019-12-19 12:12:09 +0530

    sorry

--- CoreJava_Dec18/src/com/vtalent/sathish/Sathish.java
+++ CoreJava_Dec18/src/com/vtalent/sathish/Sathish.java
@@ -2,7 +2,7 @@ package com.vtalent.sathish;
 
 public class Sathish {
 	public static void main(String[] args) {
-	 int apple[]= {10,20,30,40};
+	 int apple[]= {10,20,30,40,50};
 	 System.out.println(apple);
 	}
 		
