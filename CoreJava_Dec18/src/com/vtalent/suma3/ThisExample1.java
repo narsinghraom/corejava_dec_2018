81123c277ae04baaab96a65b46ae15ae38f558aa
sumathi92 <sumathi.vtalent@gmail.com>
2019-12-19 11:34:51 +0530

    test

--- CoreJava_Dec18/src/com/vtalent/suma3/ThisExample1.java
+++ CoreJava_Dec18/src/com/vtalent/suma3/ThisExample1.java
@@ -1,5 +1,22 @@
 package com.vtalent.suma3;
 
 public class ThisExample1 {
-
+	int x,y;
+	ThisExample1(){
+		x=5;
+		y=7;
+	
+	}
+ThisExample1(int x){
+	this.x=this.x+x;
+	y=x+y;
+}
+ThisExample1(int x, int y){
+	this.x=this.x+x;
+	this.y=this.y+y;
+}
+public static void main(String [] args) {
+	ThisExample1 t1 = new ThisExample1();
+	System.out.println();
+}
 }
