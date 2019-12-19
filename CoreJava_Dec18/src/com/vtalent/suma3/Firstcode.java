a3b42ae48f8c570e824810b475eb118430bdc8d7
sumathi92 <sumathi.vtalent@gmail.com>
2019-12-19 10:07:20 +0530

    te

---
+++ CoreJava_Dec18/src/com/vtalent/suma3/Firstcode.java
@@ -0,0 +1,13 @@
+package com.vtalent.suma3;
+
+public class Firstcode
+{
+	int i=5;
+	
+	public static void main( String args [])
+	{
+		System.out.println("SUMA");
+		System.out.println(5);
+	}
+
+}
