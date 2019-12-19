c4af01d45e6d945c3ea76964715f44ec18608b85
BhararthiVenkatalakshmi <bharathivenkatalakshmi.vtalent@gmail.com>
2019-12-19 11:36:20 +0530

    ThisExample

--- CoreJava_Dec18/src/com/vtalent/bharathi/basics/ThisExample.java
+++ CoreJava_Dec18/src/com/vtalent/bharathi/basics/ThisExample.java
@@ -1,5 +1,24 @@
 package com.vtalent.bharathi.basics;
 
 public class ThisExample {
-
+	int x,y;
+	
+	ThisExample(){
+		
+	}
+	ThisExample(int x){
+		
+	}
+	ThisExample(int x,int y){
+		
+	}
+	public static void main(String[] args) {
+		ThisExample obj1 = new ThisExample();
+		System.out.println(obj1.x);
+		
+		ThisExample obj2 = new ThisExample();
+		System.out.println(obj2.x);
+		ThisExample obj3 = new ThisExample();
+		System.out.println(obj3.x);
+	}
 }
