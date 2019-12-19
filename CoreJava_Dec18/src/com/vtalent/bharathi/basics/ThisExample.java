fd8adb8d7dc53de060444d43694c1de336053071
BhararthiVenkatalakshmi <bharathivenkatalakshmi.vtalent@gmail.com>
2019-12-19 11:49:40 +0530

    thisexp

--- CoreJava_Dec18/src/com/vtalent/bharathi/basics/ThisExample.java
+++ CoreJava_Dec18/src/com/vtalent/bharathi/basics/ThisExample.java
@@ -4,21 +4,31 @@ public class ThisExample {
 	int x,y;
 	
 	ThisExample(){
+		x= 5;
+		y = 7;
 		
 	}
 	ThisExample(int x){
+		this.x = this.x+x;
+		y = x+y;
+		
 		
 	}
 	ThisExample(int x,int y){
+		this.x = this.x+x;
+		this.y = this.y+y;
 		
 	}
 	public static void main(String[] args) {
 		ThisExample obj1 = new ThisExample();
 		System.out.println(obj1.x);
+		System.out.println(obj1.y);
 		
-		ThisExample obj2 = new ThisExample();
+		ThisExample obj2 = new ThisExample(5);
 		System.out.println(obj2.x);
-		ThisExample obj3 = new ThisExample();
+		System.out.println(obj2.y);
+		ThisExample obj3 = new ThisExample(5,6);
 		System.out.println(obj3.x);
+		System.out.println(obj3.y);
 	}
 }
