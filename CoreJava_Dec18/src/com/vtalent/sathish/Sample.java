8e11aa4553d7f608dc47d2b06712b62786444bd6
sathish <sathish.vtalent@gmail.com>
2019-12-19 11:58:32 +0530

    sample

--- CoreJava_Dec18/src/com/vtalent/sathish/Sample.java
+++ CoreJava_Dec18/src/com/vtalent/sathish/Sample.java
@@ -2,7 +2,7 @@ package com.vtalent.sathish;
 
 public class Sample {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world2");
 		
 	}
 }
