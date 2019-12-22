1c4e351d226686f44cb4eef735e9094c0565fe47
Jaya <vijaya.vtalent@gmail.com>
2019-12-22 19:46:38 +0530

    B

---
+++ CoreJava_Dec18/src/com/vtalent/vijaya/basic/B.java
@@ -0,0 +1,15 @@
+package com.vtalent.vijaya.basic;
+
+public class B {
+	B b;
+	int i;
+	B(){
+		b=new B();
+		b.i=10;
+	}
+	public static void main(String[] args) {
+		B b=new B();
+		System.out.println(b.i);
+	}
+
+}
