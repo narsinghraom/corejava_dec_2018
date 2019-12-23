5c67b4ff61c6f95de383741dc68e66680b6f65f5
sumathi92 <sumathi.vtalent@gmail.com>
2019-12-23 11:01:36 +0530

    test

---
+++ CoreJava_Dec18/src/com/vtalent/suma3/Hello.java
@@ -0,0 +1,18 @@
+package com.vtalent.suma3;
+
+public class Hello {
+	
+	String name;
+	
+	Hello()
+	{
+		this.name=" BB Book";
+	}
+	
+public static void main(String args []) {
+	
+	System.out.println("obj.name");
+		
+	}
+}
+
