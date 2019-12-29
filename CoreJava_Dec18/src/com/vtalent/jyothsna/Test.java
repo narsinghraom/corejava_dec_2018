b7938dc125c3688fbda7deda4e151498bd19da73
Jyothsna <jyothsna.vtalent@gmail.com>
2019-12-29 21:51:50 +0530

    test

--- CoreJava_Dec18/src/com/vtalent/jyothsna/Test.java
+++ CoreJava_Dec18/src/com/vtalent/jyothsna/Test.java
@@ -1,5 +1,5 @@
 package com.vtalent.jyothsna;
-class One{
+class One11{
 	int x,y,z;
 	public void function(int x) {
 		this.x=this.x+x;
@@ -11,7 +11,7 @@ class One{
 		this.y=this.y+y;
 	}
 }
-class Two extends One{
+class Two11 extends One11{
 	int x,z;
 	public void function() {
 		x=10;
@@ -21,7 +21,7 @@ class Two extends One{
 		
 	}
 }
-class Three extends Two{
+class Three11 extends Two11{
 	int z;
 	public void function(int x,int y,int z) {
 		this.x=this.x+x;
@@ -37,25 +37,25 @@ class Three extends Two{
 }
 public class Test {
 	public static void main(String[] args) {
-		Two t1=new Three();
+		Two11 t1=new Three11();
 		t1.function();
 		t1.function(5);
 		t1.function(t1.x,t1.y);
 		//t1.function(t1.x,t1.y,t1.z);
 		System.out.println(t1.x+t1.y);
-		One o1=new Three();
+		One11 o1=new Three11();
 		//o1.function();
 		o1.function(10);
 		o1.function(o1.x,o1.y);
 		//t1.function(o1.x,o1.y,o1.z);
 		System.out.println(o1.x+o1.y);
-		One on=new Two();
+		One11 on=new Two11();
 		//on.function();
 		on.function(3);
 		on.function(on.x,on.y);
 		//t1.function(on.x,on.y,on.z);
 		System.out.println(on.x+on.y);
-		Three th=new Three();
+		Three11 th=new Three11();
 		
 		th.function();
 		th.function(3);
