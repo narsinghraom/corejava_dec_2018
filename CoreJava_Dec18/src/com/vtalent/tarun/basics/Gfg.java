a25ae93b6fafc5599aa99a276a86f0e9762faa9a
Tarun <tarunkumar.vtalent@gmail.com>
2020-02-13 10:46:02 +0530

    new

---
+++ CoreJava_Dec18/src/com/vtalent/tarun/basics/Gfg.java
@@ -0,0 +1,41 @@
+package com.vtalent.tarun.basics;
+
+import java.util.Scanner;
+
+class GFG {
+	static String removeDuplicate(char str[], int n) {
+		// Used as index in the modified string
+		int index = 0;
+
+		// Traverse through all characters
+		for (int i = 0; i < n; i++) {
+
+			// Check if str[i] is present before it
+			int j;
+			for (j = 0; j < i; j++) {
+				if (str[i] == str[j]) {
+					break;
+				}
+			}
+
+			// If not present, then add it to
+			// result.
+			if (j == i) {
+				str[index++] = str[i];
+			}
+		}
+		return String.valueOf(str, index, n);
+	}
+
+	// Driver code
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the String: ");
+		String ser = s.nextLine();
+		char str[] = ser.toCharArray();
+		int n = str.length;
+		System.out.println(removeDuplicate(str, n));
+	}
+
+}
