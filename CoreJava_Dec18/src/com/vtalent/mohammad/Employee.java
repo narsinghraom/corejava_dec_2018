113bd3acdc374dab62f90cb5f3e886a1b76d4e86
Mohammad Nayeem <nayeem.vtalent@gmail.com>
2020-02-13 13:00:22 +0530

    Testing

--- CoreJava_Dec18/src/com/vtalent/mohammad/Employee.java
+++ CoreJava_Dec18/src/com/vtalent/mohammad/Employee.java
@@ -1,8 +1,21 @@
 package com.vtalent.mohammad;
 
-public class Employee {
+import java.io.Serializable;
+
+public class Employee implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	int employeeId;
 	float employeeSalary;
 	long employeeMobileNo;
+	String employeeName;
+	float yoe;//yearsOfExperience
+	double employeePackage;
+	double loanamt;
+	int tenure;
+	float emi;
+	float intrest;
 
 }
