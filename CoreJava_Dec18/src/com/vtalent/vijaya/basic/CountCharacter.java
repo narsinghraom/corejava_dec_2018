7e80de9fdcfd666b03a9f0057fb96cdee091fb6b
Jaya <vijaya.vtalent@gmail.com>
2020-02-14 09:47:35 +0530

    test

---
+++ CoreJava_Dec18/src/com/vtalent/vijaya/basic/CountCharacter.java
@@ -0,0 +1,18 @@
+package com.vtalent.vijaya.basic;
+
+public class CountCharacter {
+	public static void main(String[] args) {
+		String str="java is Programing language";
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+
+			if(str.charAt(i)!=' ')
+				count++;
+			
+		}
+		System.out.println("total no of characters in a string"+count);
+	}
+	
+
+}
