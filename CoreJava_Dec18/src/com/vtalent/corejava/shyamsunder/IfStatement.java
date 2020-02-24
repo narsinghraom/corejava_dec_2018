5df522cf28502a9227890d1187698dca8854a151
gshyamsunder <shyamsunder.vtalent@gmail.com>
2020-02-24 10:35:09 +0530

    Userstory:123

---
+++ CoreJava_Dec18/src/com/vtalent/corejava/shyamsunder/IfStatement.java
@@ -0,0 +1,18 @@
+package com.vtalent.corejava.shyamsunder;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		int age=22;
+		if(age<30){
+		
+		System.out.print("age is lessthan 30");
+		}
+		else {
+		
+			System.out.println("grt 30");
+		}
+	}
+		
+
+}
