7335a07dd02029bc039f6dc6783befe225079ac1
karthikpalukuri <karthik7r.vtalent@gmail.com>
2020-02-26 10:41:48 +0530

    examplepro

---
+++ CoreJava_Dec18/src/com/vtalent/java/karthik/Test1.java
@@ -0,0 +1,17 @@
+package com.vtalent.java.karthik;
+
+public class Test1 {
+		public static float function(float f,float f1){
+		f=f+f1;//11.0f
+		return f;
+		}
+		public static void method(int i){
+		i=i+i;//5+5=10
+		System.out.println(i);//10
+		}
+		public static void main(String []args){
+		method(5);
+		//Testthis kk=new Testthis();
+		float x=function(5.5f,5.5f);
+		System.out.println("X value is "+x);}
+		}
