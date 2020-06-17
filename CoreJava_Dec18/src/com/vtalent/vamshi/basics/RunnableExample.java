32242a5de90a27f30aa51f3f2c9376f7f4d39903
vamshi <vamshikrish71.vtalent@gmail.com>
2020-06-17 21:12:39 +0530

    Threads and Collection Task

---
+++ CoreJava_Dec18/src/com/vtalent/vamshi/basics/RunnableExample.java
@@ -0,0 +1,20 @@
+package com.vtalent.vamshi.basics;
+
+
+
+public class RunnableExample implements Runnable {
+
+	public static void main(String[] args) {
+
+		RunnableExample r1 = new RunnableExample();
+		Thread t1 = new Thread(r1);
+		t1.start();
+	}
+
+	public void run() {
+		System.out.println("This is run method");
+
+	}
+
+}
+ 
