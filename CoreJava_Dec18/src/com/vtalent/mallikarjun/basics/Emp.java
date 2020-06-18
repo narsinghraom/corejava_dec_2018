3c00b20e5925dbfcd24628261f2a105692882373
mallikarjun <mallikarjun.vtalent@gmail.com>
2020-06-18 20:30:15 +0530

    Set and Map

---
+++ CoreJava_Dec18/src/com/vtalent/mallikarjun/basics/Emp.java
@@ -0,0 +1,26 @@
+package com.vtalent.mallikarjun.basics;
+
+public class Emp {
+
+	private int id;
+
+	private String EmployeeName;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getEmployeeName() {
+		return EmployeeName;
+	}
+
+	public void setEmployeeName(String string) {
+		this.EmployeeName = EmployeeName;
+	}
+
+	public void setId(int i) {
+
+		this.id = id;
+	}
+
+}
