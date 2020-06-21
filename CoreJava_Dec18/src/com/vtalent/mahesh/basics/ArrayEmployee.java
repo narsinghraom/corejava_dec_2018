237cff52d96dde8a59d79825a7441b8c88de01a2
mahesh041 <gmahesh.vtalent@gmail.com>
2020-06-21 21:09:29 +0530

    collections

--- CoreJava_Dec18/src/com/vtalent/mahesh/basics/ArrayEmployee.java
+++ CoreJava_Dec18/src/com/vtalent/mahesh/basics/ArrayEmployee.java
@@ -5,15 +5,15 @@ import java.util.Iterator;
 
 public class ArrayEmployee {
 	
-	static ArrayList<Employee> arrayOfEmployees = new ArrayList<Employee>();
+	static ArrayList<ArrayEmployee2> arrayOfEmployees = new ArrayList<ArrayEmployee2>();
 	
 	public static void main(String[] args) {
 		Employee emp1 = new Employee();
 		emp1.setId(10);
-		emp1.setEmployeeName("Narsingh");
+		emp1.setEmployeeName("mahesh");
 		Employee emp2 = new Employee();
 		emp2.setId(20);
-		emp2.setEmployeeName("Rao");
+		emp2.setEmployeeName("gaddam");
 		arrayOfEmployees.add(emp1);
 		arrayOfEmployees.add(emp2);
 		
