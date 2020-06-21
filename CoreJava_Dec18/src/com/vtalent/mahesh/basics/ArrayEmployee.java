d11888988da6ccb7766745c1b3383fbab742a3f9
mahesh041 <gmahesh.vtalent@gmail.com>
2020-06-21 21:05:10 +0530

    Collections and threads

---
+++ CoreJava_Dec18/src/com/vtalent/mahesh/basics/ArrayEmployee.java
@@ -0,0 +1,31 @@
+package com.vtalent.mahesh.basics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayEmployee {
+	
+	static ArrayList<Employee> arrayOfEmployees = new ArrayList<Employee>();
+	
+	public static void main(String[] args) {
+		Employee emp1 = new Employee();
+		emp1.setId(10);
+		emp1.setEmployeeName("Narsingh");
+		Employee emp2 = new Employee();
+		emp2.setId(20);
+		emp2.setEmployeeName("Rao");
+		arrayOfEmployees.add(emp1);
+		arrayOfEmployees.add(emp2);
+		
+		Iterator<Employee> itr = arrayOfEmployees.iterator();
+		while(itr.hasNext()) {
+			Employee emp =itr.next();
+			System.out.println(emp.getId() + emp.getEmployeeName());
+		}
+		
+		for(Employee employee : arrayOfEmployees) {
+			System.out.println(employee.getId() + employee.getEmployeeName());
+		}
+	}
+
+}
